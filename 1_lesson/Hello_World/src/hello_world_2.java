0f151330786598b9b2a7fe3da9e3f8c12613f374
sitechbr <sitech.br@gmail.com>
2023-08-30 23:19:40 +0300

    Добавлена информация о целочисленных переменных

---
+++ 1_lesson/Hello_World/src/hello_world_2.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class hello_world_2 {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello");
+        System.out.println("World!");
+        System.out.print("Или например так Hello\n world!");
+
+
+    }
+}
---
+++ 1_lesson/variable/src/Num_variable.java
@@ -0,0 +1,31 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Num_variable {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Переменные и типы данных");
+
+        int age;
+        age = 10;
+        System.out.println("age: "+ age+".");
+
+        age = 5;
+        System.out.printf("age: %d.",age);
+
+
+
+        //byte 	8 бит 	от -128 до 127
+        //short 	16 бит 	от -32768 до 32767
+        //char 	16 бит 	беззнаковое целое число, представляющее собой символ UTF-16 (буквы и цифры)
+        //int 	32 бит 	от -2147483648 до 2147483647
+        //long 	64 бит 	от -9223372036854775808L до 9223372036854775807L
+        /*
+        Вопросы:
+        1. Возраст
+        2. Расстояния в км при путешествии по России
+        3. Размер файла на диске
+        4. Колличество страниц в интернете*/
+
+    }
+}
